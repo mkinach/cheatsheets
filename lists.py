7d562b6ee065b76a701ca8a41e1d45e97869bc7f
mpkin <mikin@physics.ubc.ca>
2021-01-02 20:18:18 -0800

    Initial commit

---
+++ lists.py
@@ -0,0 +1,25 @@
+# define some lists
+empty_list = [ ]
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+big_birds = ['emu', 'ostrich', 'cassowary']
+first_names = ['Graham', 'John', 'Terry', 'Terry', 'Michael']
+
+# you can also make an empty list with the list() function
+another_empty_list = list()
+print(another_empty_list)   # []
+
+# convert a string to a list
+print(list('cat'))   # ['c', 'a', 't']
+
+# convert a tuple to a list
+a_tiple = ('ready', 'fire', 'aim')
+print(list(a_tiple))
+#['ready', 'fire', 'aim']
+
+# access elements in a list
+marxes = ['Grucho', 'Chico', 'Harpo']
+print(marxes[0])  #Groucho
+print(marxes[-1]) #Harpo
+
+# lists of lists
+
